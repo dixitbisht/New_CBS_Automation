1d5d8a272d330ddd581565891bace89733adc08b
dixit <dixitbisht99@gmail.com>
2025-09-13 00:44:12 +0530

    Change loading

--- src/main/java/com/CBS/Base/TestBase.java
+++ src/main/java/com/CBS/Base/TestBase.java
@@ -1,15 +1,24 @@
 package com.CBS.Base;
 
 import com.CBS.Util.TestUtil;
+import org.apache.commons.exec.CommandLine;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.http.Route;
+
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestBase {
    public static WebDriver driver;
    public static Properties prop;
+   public static ChromeOptions options;
 
     public TestBase() {
 try{
@@ -24,10 +33,30 @@ catch (Exception e) {
 
     public static void intitlisation() {
         String BrowserName = prop.getProperty("Browser");
-        if (BrowserName.equals("Chrome")){
-       System.setProperty("webdriver.chrome.driver", "/home/appinventiv/Desktop/CBS_Automation/chromedriver-linux64/chromedriver");
-       driver = new ChromeDriver();
+        if (BrowserName.equals("Chrome")) {
+            System.setProperty("webdriver.chrome.driver", "/home/appinventiv/Desktop/CBS_Automation/chromedriver-linux64/chromedriver");
+            //driver = new ChromeDriver();
+            options = new ChromeOptions();
+
+//            else if(BrowserName.equals("firefox")){
+//                System.setProperty("webdriver.gecko.driver", "/home/appinventiv/Desktop/CBS_Automation/geckodriver-v0.36.0-linux64/geckodriver");
+//              driver  = new FirefoxDriver();
+//
+//            }
+
+        try {
+            Path tempProfileDir = Files.createTempDirectory("chrome-profile");
+            options.addArguments("--user-data-dir=" + tempProfileDir.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        // Recommended for Jenkins/Linux
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        ///options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
+    }
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(TestUtil.Page_Load_timeout, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(TestUtil.implicitwait, TimeUnit.SECONDS);
